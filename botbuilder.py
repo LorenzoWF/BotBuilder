1c5b6ecbc8f383166466081d975fd9ce9b327bf6
LorenzoWF <lorenzo.wendt@outlook.com>
2018-05-20 16:40:16 -0300

    Ajustado ip

--- botbuilder.py
+++ botbuilder.py
@@ -32,4 +32,4 @@ def testePost():
 #PARTE4
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
-  app.run(host='172.18.30.174', port=port)
+  app.run(host='127.0.0.1', port=port)
