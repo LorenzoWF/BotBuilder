9ddf622c17fdd31e203a1db36fca53768b0f5c12
LorenzoWF <lorenzo.wendt@outlook.com>
2018-05-21 13:09:37 -0300

    TESTE COM JSON PADRAO

--- botbuilder.py
+++ botbuilder.py
@@ -1,37 +1,61 @@
-#PARTE1
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, safe_join, send_from_directory
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 import os
 
-#PARTE2
 app = Flask(__name__)
 
-#PARTE3
+@app.route('/')
+def index():
+    return render_template("index.html")
+
 @app.route('/teste', methods=['GET'])
 def testeGet():
-  #MEU CÓDIGO AQUI
   print("teste")
   return"Tudo pronto!"
 
 @app.route('/teste', methods=['POST'])
 def testePost():
-  #MEU CÓDIGO AQUI
   content = request.json
-  #content = request.args['idade']
-  json_response = {"messages": [
-                        {"text": "Tu tem " + content['idade'] + " anos"},
-                        {"text": content['idade'] + " centrimetros de altura"},
-                        {"text": "e pesa " + content['peso'] + " quilos"}
+  #json_response = {"messages": [
+    #                    {"text": "Tu tem " + content['idade'] + " anos"},
+    #                    {"text": content['idade'] + " centrimetros de altura"},
+    #                    {"text": "e pesa " + content['peso'] + " quilos"}
+    #                ]
+    #              }
+
+  json_response = [{
+                    "attachment":{
+                    "type":"template",
+                    "payload":{
+                    "template_type":"generic",
+                    "elements":[
+                    {
+                    "title":"Cajeros Bbva",
+                    "subtitle":"Reconocida entidad financiera requiere estudiantes activos de 5 semestre en adelante de JORNADA DIURNA/NOCTURNO/ RECIÉN EGRESADOS en carreras administrativas ...",
+                    "buttons":[
+                    {
+                    "type":"web_url",
+                    "url":"https://www.facebook.com/oferta/c2fa78cfdbc21585",
+                    "title":"Aplicar"
+                    },
+                    {
+                    "type":"web_url",
+                    "url":"https://www.facebook.com/oferta/c2fa78cfdbc21585",
+                    "title":"Mas Informacion"
+                    }
                     ]
-                  }
+                    }
+                    ]
+                    }
+                    }
+                    }]
 
   return jsonify(json_response)
-  #return jsonify(content)
 
-#PARTE4
 if __name__ == '__main__':
-  #port = int(os.environ.get('PORT', 5000))
-  #app.run(host='localhost', port=port)
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='192.168.9.206', port=port)
   #app.run(host='localhost')
-  app.run()
+  #app.run(host='192.168.9.206')
+  #app.run()
